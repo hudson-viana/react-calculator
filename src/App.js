2c587e1a5e0eb94c79dfb31c2f2ac53271994045
Hudson Viana <110697704+hudson-viana@users.noreply.github.com>
2024-01-26 20:13:21 +0000

    feat: add calculator new css design, and add new calculator html

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
-const App = () => {};
+const App = () => {
+  return (
+    <div className="App">
+      <div className="calculator">
+        <div className="display">
+          <span>(0)</span> 0
+        </div>
+        <div className="operators">
+          <button className="operators-symbol">÷</button>
+          <button className="operators-symbol">×</button>
+          <button className="operators-symbol">-</button>
+          <button className="operators-symbol">+</button>
+          <button >DEL</button>
+        </div>
+        <div className="digits">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
