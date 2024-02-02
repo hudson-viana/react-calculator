f4e73c4b0d9b239c905e4b341799ef1a557b16d9
Hudson Viana <110697704+hudson-viana@users.noreply.github.com>
2024-02-02 11:51:53 +0000

    feat: add features of delete, calculate,  and calculator app is now working

--- src/App.js
+++ src/App.js
@@ -1,30 +1,75 @@
+import { useState } from "react";
+
 const App = () => {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ["/", "*", "+", "-", "."];
+
+  const updateCalc = (value) => {
+    // to avoid duplicated operators
+    if (
+      (ops.includes(value) && calc === "") ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+
+    setCalc(calc + value);
+
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  };
+
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  };
+
+  const deleteLast = () => {
+    if (calc == "") {
+      return;
+    }
+    const value = calc.slice(0, -1);
+
+    setCalc(value);
+  };
+
   return (
     <div className="App">
       <div className="calculator">
         <div className="display">
-          <span>(0)</span> 0
+          {result ? <span>({result})</span> : ""}&nbsp;
+          {calc || "0"}
         </div>
         <div className="operators">
-          <button className="operators-symbol">÷</button>
-          <button className="operators-symbol">×</button>
-          <button className="operators-symbol">-</button>
-          <button className="operators-symbol">+</button>
-          <button >DEL</button>
+          <button className="operators-symbol" onClick={() => updateCalc("/")}>
+            ÷
+          </button>
+          <button className="operators-symbol" onClick={() => updateCalc("*")}>
+            ×
+          </button>
+          <button className="operators-symbol" onClick={() => updateCalc("-")}>
+            -
+          </button>
+          <button className="operators-symbol" onClick={() => updateCalc("+")}>
+            +
+          </button>
+          <button onClick={() => deleteLast()}>DEL</button>
         </div>
         <div className="digits">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>.</button>
-          <button>=</button>
+          <button onClick={() => updateCalc("7")}>7</button>
+          <button onClick={() => updateCalc("8")}>8</button>
+          <button onClick={() => updateCalc("9")}>9</button>
+          <button onClick={() => updateCalc("4")}>4</button>
+          <button onClick={() => updateCalc("5")}>5</button>
+          <button onClick={() => updateCalc("6")}>6</button>
+          <button onClick={() => updateCalc("1")}>1</button>
+          <button onClick={() => updateCalc("2")}>2</button>
+          <button onClick={() => updateCalc("3")}>3</button>
+          <button onClick={() => updateCalc("0")}>0</button>
+          <button onClick={() => updateCalc(".")}>.</button>
+          <button onClick={() => calculate()}>=</button>
         </div>
       </div>
     </div>
